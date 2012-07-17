28353ac1afecca16013d0eaef7ab01a06f5dd65d
Leonidas Esteban <leonidasesteban@gmail.com>
2012-07-17 11:12:30 -0500

    imagenes de estilos /conferencia

---
+++ static/js/codiguito-conferencia.js
@@ -0,0 +1,45 @@
+﻿$(document).ready(
+	function ()
+	{
+		
+		var opciones = {
+			beforeSubmit: enviando,
+			success: recepcion,
+			clearForm: true
+		};
+		$('#formulario').ajaxForm(opciones); 
+	}
+	
+);
+function enviando()
+{
+	$("#inscripcion h3").text("Inscribiendote...").fadeOut().fadeIn();
+}
+
+function recepcion(datos)
+{
+	datos = $.trim(datos);
+	if(datos=="OK")
+	{
+		$("#inscripcion h3").text("¡Ya estás inscrito! :D").fadeOut().fadeIn();
+		$("#inscripcion #confirmacion").slideDown();
+		var translacion = "translate(400px, 250px)"; 
+		var transform = "transform: " + translacion + "; " + 
+						"-webkit-transform: " + translacion + "; " + 
+						"-moz-transform: " + translacion + "; " + 
+						"-ms-transform: " + translacion + "; " + 
+						"-o-transform: " + translacion + ";"; 
+		/*$("#social").attr("style", "margin: 150px 0 0 200px !important; " + transform);*/
+		$('.btn_twitter,.btn_facebook,.btn_googlemas').css('opacity',1);
+		
+	}
+	else
+	{
+		$("#inscripcion h3").text("Algo falló :(").fadeOut().fadeIn();
+		$("#inscripcion #confirmacion").text("Verifica que todos los datos estén bien escritos").slideDown();
+		$("#formulario #nombre").focus();
+	}
+	
+	
+	
+}
